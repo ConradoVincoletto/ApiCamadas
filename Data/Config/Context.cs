b6cf8ad8a7794e7e23a28b7bd88df0e8e81101c4
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-05-05 18:14:18 -0300

    feat: classe context para o banco de dados

---
+++ Data/Config/Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Categoria;
+using Model.Produto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Config
+{
+    public class Context : DbContext
+    {
+        protected Context()
+        {
+        }
+        public Context(DbContextOptions options) : base(options)
+        {
+
+        }
+        DbSet<ProdutoViewModel> Produtos { get; set; }
+        DbSet<CategoriaViewModel> Categorias { get; set; }
+    }
+}
