74611db475c7d66e302fab480a9686b4597eaf1a
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-05-06 19:50:56 -0300

    feat: Controller Produto

---
+++ ProjetoTrêsCamadas/Controllers/ProdutosController.cs
@@ -0,0 +1,54 @@
+﻿using Azure.Core;
+using Business.InterfaceProduto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using Model.Produto;
+
+namespace ProjetoTrêsCamadas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : Controller
+    {
+        private readonly IProduto _IProduto;
+
+        public ProdutosController(IProduto IProduto)
+        {
+            _IProduto= IProduto;
+        }
+
+        [HttpGet("ListasProdutos")]
+        public async Task<IActionResult> ListasProdutos()
+        {
+            return Json(await _IProduto.List());
+        }
+
+        [HttpPost("AdicionarProduto")]
+        public async Task AdicionarProduto(ProdutoViewModel produto)
+        {
+            await _IProduto.Add(produto);
+        }
+
+        [HttpGet("ObterProdutoPorId")]
+        public async Task<IActionResult> ObterProdutoPorId(int Id)
+        {
+            return Json(await _IProduto.GetEntityById(Id));
+        }
+
+
+
+        [HttpPut("AtualizarProduto")]
+        public async Task AtualizarProduto(ProdutoViewModel produto)
+        {
+            await _IProduto.Update(produto);
+        }
+
+        [HttpDelete("RemoverProduto")]
+        public async Task RemoverProduto(ProdutoViewModel produto)
+        {
+            await _IProduto.Delete(produto);
+        }
+
+    }
+}
