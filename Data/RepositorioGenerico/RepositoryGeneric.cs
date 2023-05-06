60ae4510f750389b6517ceaceb4d36471d08a370
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-05-06 13:40:06 -0300

    feat: Repositório Generic com as tasks configuradas

---
+++ Data/RepositorioGenerico/RepositoryGeneric.cs
@@ -0,0 +1,99 @@
+﻿using Business.InterfaceGenerica;
+using Data.Config;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.RepositorioGenerico
+{
+    public class RepositoryGeneric<T> : IGeneric<T>, IDisposable where T : class
+    {
+        private readonly DbContextOptions<Context> _OptionBuilder;
+        public RepositoryGeneric()
+        {
+            _OptionBuilder= new DbContextOptions<Context>();
+        }
+
+        public async Task Add(T Objeto)
+        {
+            using (var data = new Context(_OptionBuilder))
+            {
+                await data.Set<T>().AddAsync(Objeto);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete(T Objeto)
+        {
+            using (var data = new Context(_OptionBuilder))
+            {
+                data.Set<T>().Remove(Objeto);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task<T> GetEntityById(int Id)
+        {
+            using (var data = new Context(_OptionBuilder))
+            {
+                return await data.Set<T>().FindAsync(Id);
+                
+            }
+        }
+
+        public async Task<List<T>> List()
+        {
+            using (var data = new Context(_OptionBuilder))
+            {
+                return await data.Set<T>().ToListAsync();
+                
+            }
+        }
+
+        public async Task Update(T Objeto)
+        {
+            using (var data = new Context(_OptionBuilder))
+            {
+                data.Set<T>().Update(Objeto);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        bool disposed = false;
+
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        
+
+        public void Dispose()
+        {
+            // Dispose of unmanaged resources.
+            Dispose(true);
+            // Suppress finalization.
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                handle.Dispose();
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+            // TODO: set large fields to null.
+
+            disposed = true;
+        }
+    }
+}
