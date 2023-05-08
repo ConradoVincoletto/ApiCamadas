9789996093d1b744549288a71daab7a22a2af24e
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-05-08 17:27:54 -0300

    feat: Repositorio Categoria utilizando ID com o RepositoryGeneric

---
+++ Data/CategotiaRepositorio/RepositorioCategoria.cs
@@ -0,0 +1,22 @@
+﻿using Data.Config;
+using Data.RepositorioGenerico;
+using Microsoft.EntityFrameworkCore;
+using Model.Categoria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.CategotiaRepositorio
+{
+    public class RepositorioCategoria : RepositoryGeneric<CategoriaViewModel>
+    {
+        private readonly DbContextOptions<Context> _OptionBuilder;
+
+        public RepositorioCategoria()
+        {
+            _OptionBuilder= new DbContextOptions<Context>();
+        }
+    }
+}
